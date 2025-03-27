4d7deaeaf9851650650d4a794a17ba74500c7692
AdrianRV <63867908+zu0gg@users.noreply.github.com>
2025-03-26 20:51:12 -0600

    Commit comienzo

---
+++ SodaAntojeriaTica/SodaAntojeriaTica/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SodaAntojeriaTica.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult IniciarSesion()
+        {
+            return View();
+        }
+
+        public IActionResult Principal()
+        {
+            return View();
+        }
+    }
+}
