76a9571268321ec3764f1b7cacddad8a3a0b128b
Sebastian_yrb <123045887+ReyesSebass@users.noreply.github.com>
2025-03-29 01:47:12 -0600

    Se agregaron las vistas de ConusltarUsuario,EditarUsuario y AgregarUsuario al proyecto WEB, aparte de carpeta de Dependencias

---
+++ SodaAntojeriaTica/SodaAntojeriaTica/Dependencias/Utilitarios.cs
@@ -0,0 +1,22 @@
+﻿namespace SodaAntojeriaTica.Dependencias
+{
+    public class Utilitarios
+    {
+        private readonly IHttpClientFactory _httpClient;
+        private readonly IConfiguration _configuration;
+        private readonly IHttpContextAccessor _accessor;
+        public Utilitarios(IHttpClientFactory httpClient, IConfiguration configuration, IHttpContextAccessor accessor)
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+            _accessor = accessor;
+        }
+
+        public HttpResponseMessage ConsultarInfoUsuarios(int id)
+        {
+            var url = _configuration["UrlApi"] + "api/Usuarios/ListarUsuarios";
+            var client = _httpClient.CreateClient();
+            return client.GetAsync(url).Result;
+        }
+    }
+}
--- SodaAntojeriaTica/SodaAntojeriaTica/Program.cs
+++ SodaAntojeriaTica/SodaAntojeriaTica/Program.cs
@@ -10,14 +10,12 @@ builder.Services.AddSession(); // Agregar servicio de sesión
 
 var app = builder.Build();
 
-// Habilitar middleware de sesión
+
 app.UseSession();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
